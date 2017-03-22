d2d43185924c1e9c49e26c42f9b5e73a0832b10a
anoff <offenhaeuser@gmail.com>
2017-03-22 22:38:31 +0100

    :star2: feat: plot elevation colored by delta

---
+++ plot.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import gpxpy.parser as parser
+import datetime
+import numpy as np
+
+gpx_file = open( 'data/runtastic_20170322_1146_Skiing.gpx', 'r' )
+
+gpx_parser = parser.GPXParser( gpx_file )
+gpx = gpx_parser.parse()
+
+gpx_file.close()
+
+print("Tracks: {}, Waypoints: {}, Routes: {}".format(len(gpx.tracks), len(gpx.waypoints), len(gpx.routes)))
+
+
+#points = [p for p in s for s in t.segments for t in gpx.tracks]
+points = [[seg.points for seg in t.segments] for t in gpx.tracks]
+print(len(gpx.tracks[0].segments[0].points))
+points = []
+for track in gpx.tracks:
+    for segment in track.segments:
+        for point in segment.points:
+            points.append(point)
+print(len(points))
+times = [p.time for p in points]
+t_min = min(times)
+durations = [(t - t_min).total_seconds() for t in times]
+heights = [p.elevation for p in points]
+delta_h = np.append(0, np.diff(heights))
+
+print(len(delta_h), len(durations))
+#plt.plot(durations, heights, c='b')
+#plt.plot(durations, delta_h*100, c='r')
+for ix in range(len(durations)):
+    if delta_h[ix] > 0:
+        c = 'r'
+    elif delta_h[ix] == 0:
+        c = 'y'
+    else:
+        c = 'b'
+    plt.scatter(durations[ix], heights[ix], c=c)
+
+plt.show()
